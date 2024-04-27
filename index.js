f6d8392bf1519093aa3755bc9e52829e4346edb4
hasanali207 <hasanali142228@gmail.com>
2024-04-27 13:36:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT  || 5000
+
+app.use(express.json())
+app.use(cors())
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://artisan-avenue:artisan-avenue@cluster0.wdiwjgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("craftItem");
+    const itemCollection = database.collection("Items");
+    
+    app.post('/items', async(req, res) =>{
+        const newItem = req.body
+        console.log(newItem)
+        const result = await itemCollection.insertOne(newItem)
+        res.send(result)
+    })
+
+
+    app.get('/items', async(req, res)=>{
+        const cursor = itemCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+   
+
+    app.get('/items/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await itemCollection.findOne(query)
+      res.send(result)
+    })
+
+
+    app.get('/craftlist/:email', async(req, res)=>{
+      const email = req.params.email
+      const query = {user_email : email}
+      const result = await itemCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', async(req, res) =>{
+    res.send('Start Server ')
+})
+
+app.listen(port, () =>{
+    console.log(`Server is Runnig ${port}` );
+    
+})
